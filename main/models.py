7dd31c7f354ef3d91951f99d0547903cb81f7d1e
Martin Nečas <necas.marty@gmail.com>
2022-07-23 20:42:51 +0200

    Add favicon.ico

--- main/models.py
+++ main/models.py
@@ -2,20 +2,14 @@ from django.db import models
 
 
 class Honeypot(models.Model):
+    name = models.CharField(max_length=64)
     type = models.CharField(max_length=64)
+    ip = models.GenericIPAddressField()
 
     def __str__(self):
-        return f"{self.type}"
+        return f"{self.name} - {self.type}"
 
 
 class HoneypotData(models.Model):
     data = models.JSONField()
-
-
-class HoneypotStruct(models.Model):
     honeypot = models.ForeignKey(Honeypot, on_delete=models.CASCADE)
-    type = models.CharField(max_length=64)
-    name = models.CharField(max_length=64)
-
-    def __str__(self):
-        return f"{self.type}"
