218a28648988fc0a92802eb570e1dd871e05ec5e
Martin Nečas <necas.marty@gmail.com>
2022-07-23 21:23:51 +0200

    Add api basic

--- main/urls.py
+++ main/urls.py
@@ -5,4 +5,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index),
+    path('api/honeypot', views.honeypot),
+    path('api/data', views.data),
 ]
--- main/views.py
+++ main/views.py
@@ -1,6 +1,19 @@
 from django.shortcuts import render
+from django.core import serializers
+from django.http import HttpResponse
+from main.models import Honeypot, HoneypotData
 
 
 def index(request):
     if request.method == 'GET':
         return render(request, 'index.html', {})
+
+def honeypot(request):
+    if request.method == 'GET':
+        data = serializers.serialize('json', Honeypot.objects.all())
+        return HttpResponse(data, content_type='application/json')
+
+def data(request):
+    if request.method == 'GET':
+        data = serializers.serialize('json', HoneypotData.objects.all())
+        return HttpResponse(data, content_type='application/json')
