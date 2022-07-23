10ac8117931adf9801b627710ad6551a9483e6ce
Martin Nečas <necas.marty@gmail.com>
2022-07-23 19:27:01 +0200

    Add templates and static

--- main/models.py
+++ main/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+
+class Honeypot(models.Model):
+    type = models.CharField(max_length=64)
+
+    def __str__(self):
+        return f"{self.type}"
+
+
+class HoneypotData(models.Model):
+    data = models.JSONField()
+
+
+class HoneypotStruct(models.Model):
+    honeypot = models.ForeignKey(Honeypot, on_delete=models.CASCADE)
+    type = models.CharField(max_length=64)
+    name = models.CharField(max_length=64)
+
+    def __str__(self):
+        return f"{self.type}"
