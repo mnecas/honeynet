b381d924e62f8c90403f5944ce5f3f9cad8e991b
Martin Nečas <necas.marty@gmail.com>
2022-07-23 19:57:42 +0200

    Add managment and docker

---
+++ main/management/commands/create_admin.py
@@ -0,0 +1,18 @@
+from django.core.management.base import BaseCommand, CommandError
+from django.contrib.auth.models import User
+
+class Command(BaseCommand):
+
+    def handle(self, *args, **options):
+
+        # The magic line
+        if not User.objects.filter(username='mnecas'):
+            user = User.objects.create_user(
+                username= 'mnecas',
+                email='mnecas@mnecas.cz',
+                is_staff=True,
+                is_active=True,
+                is_superuser=True
+            )
+            user.set_password('mnecas')
+            user.save()
