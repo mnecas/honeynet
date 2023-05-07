c21c3eb8113e14ea91f2f2f5644d325df4505449
Martin Nečas <necas.marty@gmail.com>
2023-05-07 16:38:25 +0200

    Add remote database
    Signed-off-by: Martin Nečas <necas.marty@gmail.com>

--- web_server/settings.py
+++ web_server/settings.py
@@ -83,9 +83,17 @@ WSGI_APPLICATION = "web_server.wsgi.application"
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    # "default": {
+    #     "ENGINE": "django.db.backends.sqlite3",
+    #     "NAME": BASE_DIR / "db.sqlite3",
+    # },
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('POSTGRES_NAME'),
+        'USER': os.environ.get('POSTGRES_USER'),
+        'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
+        'HOST': 'localhost',
+        'PORT': 5432,
     }
 }
 
